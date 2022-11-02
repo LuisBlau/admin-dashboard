6284e0c4718adf61a370ef800d3ede0dfc927b36
LuisTay <luisproteamvn@outlook.com>
2022-11-03 04:44:36 +0700

    initial

---
+++ src/app/modules/ECommerce/pages/eCommercePage.js
@@ -0,0 +1,29 @@
+import React, { Suspense } from "react";
+import { Redirect, Switch } from "react-router-dom";
+import { UsersPage } from "./users/CustomersPage";
+import { CustomersPage } from "./customers/CustomersPage";
+import { TournamentsPage } from "./tournaments/CustomersPage";
+
+// import { ProductsPage } from "./products/ProductsPage";
+// import { ProductEdit } from "./products/product-edit/ProductEdit";
+import { LayoutSplashScreen, ContentRoute } from "../../../../_metronic/layout";
+
+export default function eCommercePage() {
+  return (
+    <Suspense fallback={<LayoutSplashScreen />}>
+      <Switch>
+        {
+          /* Redirect from eCommerce root URL to /customers */
+          <Redirect
+            exact={true}
+            from="/management"
+            to="/management/users"
+          />
+        }
+        <ContentRoute path="/management/users" component={UsersPage} />
+        <ContentRoute path="/management/customers" component={CustomersPage} />
+        <ContentRoute path="/management/tournaments" component={TournamentsPage} />
+      </Switch>
+    </Suspense>
+  );
+}
